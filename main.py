9622c0a3469efc901768f0c59e38fad36d6493c0
lorenzoo22 <91331188+lorenzoo22@users.noreply.github.com>
2022-09-23 17:17:49 +0200

    +cambios3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #lanzador
 from Ejercicios import Ejercicio1,Ejercicio2,Ejercicio3, tabla #importacion de los modulos
 from Ejercicios.Ejercicio1 import Ejercicio1  #importacion
-# from Ejercicios.Ejercicio2 import Ejercicio2  #de
+from Ejercicios.Ejercicio2 import Ejercicio2  #de
 # from Ejercicios.Ejercicio3 import Ejercicio3  #las
 from Ejercicios.tabla import tabla            #clases
 
@@ -36,7 +36,27 @@ def pruebas():
             print("[3,3,3,9]")
             print("[4,4,4,13]")
             print(Ejercicio1.sumaMatrices(matriz))
-        if opcion == 
+        if opcion == "2":
+            print("Ejercicio_2 con los siguientes strings...\n")ç
+            print("Hola")
+            print("nochevieja")
+            print("fun__fun__fun")
+            print("adios")
+            Ejercicio2.Ejericio2.cadena("Hola")
+            Ejercicio2.Ejericio2.cadena("nochevieja")
+            Ejercicio2.Ejericio2.cadena("fun__fun__fun")
+            Ejercicio2.Ejericio2.cadena("adios")
+
+        if opcion == "3":
+            0-10
+            -10,0
+            0,20--pares
+            -20,0--impares
+            0,50---5
+            
+        if opcion == "4":
+
+
 
 
 
