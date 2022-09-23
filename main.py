da14c807d3c57b6c88fc029a4f2b2fa351c2ff48
lorenzoo22 <91331188+lorenzoo22@users.noreply.github.com>
2022-09-23 16:51:24 +0200

    cambios_main

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 #lanzador
-from Ejercicios import Ejercicio1,Ejercicio2,Ejercicio3, tabla
+from Ejercicios import Ejercicio1,Ejercicio2,Ejercicio3, tabla #importacion de los modulos
+from Ejercicios.Ejercicio1 import Ejercicio1  #importacion
+from Ejercicios.Ejercicio2 import Ejercicio2  #de
+from Ejercicios.Ejercicio3 import Ejercicio3  #las
+from Ejercicios.tabla import tabla  #clases
+
+
 
 
 
 
 def pruebas():
+        matriz = [
+        [1,1,1,3],
+        [2,2,2,7],
+        [3,3,3,9],
+        [4,4,4,13],
+        ]
+        Ejercicio1.Ejercicio1
 
 if __name__ == "main":
     pruebas() 
